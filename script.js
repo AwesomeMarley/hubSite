387d408e31dffca9b386ef071ff7d0164aeeb21e
Brandon <86384607+AwesomeMarley@users.noreply.github.com>
2022-03-24 14:17:09 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+/*
+const example_element = document.createElement('div');
+example_element.id = 'example'
+example_element.text_content = 'your text here';
+document.body.append(example_element);
+*/
+
+
+
+// const web_tab = document.createElement('div');
+// web_tab.id = 'web_element';
+// web_tab.textContent = 'Web Browser';
+// document.body.append(web_tab);
+
+// const games_tab = document.createElement('div');
+// games_tab.id = 'games_element';
+// games_tab.textContent = 'Games';
+// document.body.append(games_tab);
+
+// const utilites_tab = document.createElement('div');
+// utilites_tab.id = 'utilities_element';
+// utilites_tab.textContent = 'Utilities';
+// document.body.append(utilites_tab);
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
